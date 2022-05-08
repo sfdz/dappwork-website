95d3c93c8a85eba2b97b8acdcad567105de9ebf3
Sam Fernandez <sfdz@users.noreply.github.com>
2022-05-08 15:49:30 -0400

    Preliminary react-router-dom work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import ClaimBounty from './components/ClaimBounty';
+import NewBounty from './components/NewBounty';
+import OpenBounties from './components/OpenBounties';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>DAppWork</h1>
-        <ClaimBounty githubUsername='sfdz'/>
+        <Routes>
+          <Route path="/" element={<OpenBounties />} />
+          <Route path="/new-bounty" element={<NewBounty />} />
+          <Route path="/bounty/:id" element={<ClaimBounty githubUsername='sfdz' />} />
+        </Routes>
       </header>
     </div>
   );
